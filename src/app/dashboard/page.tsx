83e461582eb13bef3f412c782a6c04f0c32572ec
Kacper Zelichowski <kacper.zelichowski93@gmail.com>
2025-07-05 19:20:45 +0200

    feat: display all opportunites

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,37 @@
-"use server";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { prisma } from "@/config/prisma.config";
 
 export default async function Dashboard() {
+  const opportunites = await prisma.opportunity.findMany();
+
   return (
-    <div className="">
+    <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 ">
+      {opportunites.map((opportunity) => (
+        <Card
+          key={opportunity.id}
+          className="m-3 h-[300px] hover:cursor-pointer hover:bg-gray-100 hover:shadow-xl"
+        >
+          <CardHeader>
+            <CardTitle>{opportunity.title}</CardTitle>
+          </CardHeader>
+          <CardContent className="h-1/2 overflow-y-hidden">
+            {opportunity.description}
+          </CardContent>
+          <CardFooter className="flex justify-between w-4/5">
+            Save the date:
+            <strong>
+              {` ${opportunity.date.toLocaleDateString()}`}
+              {` ${opportunity.date.toLocaleTimeString()}`}
+            </strong>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex bg-gray-100 items-start justify-center min-h-screen p-4 pb-20 sm:p-10 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col justify-self-center items-center justify-center rounded-md border-2 p-10 md:max-w-3/4 lg:max-w-2/5">
-        <div className="mb-8 text-3xl">Welcome to VolunteerHub!</div>
+        <div className="mb-8 text-3xl text-center">Welcome to VolunteerHub!</div>
         <div className="text-center">
           VolunteerHub is a web application that connects volunteers with local
           non-profit organizations and community initiatives. Volunteers can
