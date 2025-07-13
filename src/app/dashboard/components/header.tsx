f3b9b56aa309ee5a97891bdb54263ae4799a926c
Kacper Zelichowski <kacper.zelichowski93@gmail.com>
2025-07-13 13:03:27 +0200

    feat: simplify theme-toggle

--- src/app/dashboard/components/header.tsx
+++ src/app/dashboard/components/header.tsx
@@ -25,38 +25,45 @@ export default function Header({ user }: HeaderProps) {
   };
 
   return (
-    <header className="flex justify-between items-center p-6 dark:bg-gray-900 bg-gray-700 text-white">
+    <header className="flex justify-between items-center p-6 bg-zinc-600 text-white">
       <strong>VolunteerHub</strong>
 
       <div className="flex items-center">
         <ThemeToggle />
 
-        <strong className="ml-4">{user?.email}</strong>
+        {user && (
+          <>
+            <strong className="ml-4">{user.email}</strong>
 
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Menu className="ml-4" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="p-4 my-1 mr-6">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <Link href={"/dashboard/profile"}>
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-              </Link>
-              {user?.role === Role.VOLUNTEER && (
-                <DropdownMenuItem>My Applications</DropdownMenuItem>
-              )}
-              {user?.role === Role.ORGANIZATION && (
-                <DropdownMenuItem>Opportunites</DropdownMenuItem>
-              )}
-              <DropdownMenuSeparator />
-              <DropdownMenuItem variant="destructive" onClick={handleLogout}>
-                Log out
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Menu className="ml-4" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="p-4 my-1 mr-6">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <Link href={"/dashboard/profile"}>
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                  </Link>
+                  {user.role === Role.VOLUNTEER && (
+                    <DropdownMenuItem>My Applications</DropdownMenuItem>
+                  )}
+                  {user.role === Role.ORGANIZATION && (
+                    <DropdownMenuItem>Opportunites</DropdownMenuItem>
+                  )}
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem
+                    variant="destructive"
+                    onClick={handleLogout}
+                  >
+                    Log out
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
+        )}
       </div>
     </header>
   );
--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -1,18 +1,8 @@
 "use client";
 
-import { useTheme } from "next-themes";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuSeparator,
-} from "../ui/dropdown-menu";
-import {
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { LucideMoon, LucideSun } from "lucide-react";
-import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
+import { LucideMoon, LucideSun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 type ThemeToggleProps = {
   className?: string;
@@ -22,29 +12,17 @@ export default function ThemeToggle({ className }: ThemeToggleProps) {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild className={cn(className)}>
-        <Button size="icon">
-          <LucideSun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <LucideMoon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem
-          className="flex justify-center py-2"
-          onClick={() => setTheme("dark")}
-        >
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className="flex justify-center py-2"
-          onClick={() => setTheme("light")}
-        >
-          Light
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className={cn("cursor-pointer", className)}>
+      <LucideSun
+        width="1.2rem"
+        className="rotate-0 transition-all dark:hidden hover:animate-pulse"
+        onClick={() => setTheme("dark")}
+      />
+      <LucideMoon
+        width="1.2rem"
+        className="dark:rotate-0 hidden dark:block hover:animate-pulse"
+        onClick={() => setTheme("light")}
+      />
+    </div>
   );
 }
